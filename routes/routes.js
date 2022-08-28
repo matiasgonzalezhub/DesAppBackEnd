15165b890b20cf516408f0b4816128e5ddbb48cd
Matias Gonzalez <matias.gonzalez@live.com.ar>
2022-08-28 10:32:34 -0300

    se elimina codigo de base de ejemplo

--- routes/routes.js
+++ routes/routes.js
@@ -1,50 +1,6 @@
 const express = require("express");
-
 const router = express.Router();
-
-const Model = require("../models/model");
 const Equipo = require("../models/equipo");
-
-//Post Method
-router.post("/post", async (req, res) => {
-  // res.send('Post API')
-  const data = new Model({
-    name: req.body.name,
-    age: req.body.age,
-  });
-
-  try {
-    const dataToSave = await data.save();
-    res.status(200).json(dataToSave);
-  } catch (error) {
-    res.status(400).json({ message: error.message });
-  }
-});
-
-//Get all Method
-router.get("/getAll", (req, res) => {
-  res.send("Get All API");
-});
-
-//Get by ID Method
-router.get("/getOne/:id", (req, res) => {
-  res.send("Get by ID API");
-});
-
-//Update by ID Method
-router.patch("/update/:id", (req, res) => {
-  res.send("Update by ID API");
-});
-
-//Delete by ID Method
-router.delete("/delete/:id", (req, res) => {
-  res.send("Delete by ID API");
-});
-
-//Get all Method
-router.get("/getAll", (req, res) => {
-  res.send("Get All elementos from API");
-});
 
 //Verbos para equipos
 //Post de un equipo
@@ -104,7 +60,9 @@ router.delete("/equipo/delete/:id", async (req, res) => {
   try {
     const id = req.params.id;
     const data = await Equipo.findByIdAndDelete(id);
-    res.send(`Equipo con descripcion ${data.descripcion} y id ${id} fue eliminado correctamente`);
+    res.send(
+      `Equipo con descripcion ${data.descripcion} y id ${id} fue eliminado correctamente`
+    );
   } catch (error) {
     res.status(400).json({ message: error.message });
   }
